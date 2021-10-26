5edbe5d4300557057aa763e5d2ce32afe37ec42b
CKahveci <cansukahveci94@outlook.com>
2021-10-27 01:01:02 +0300

    created context

---
+++ WInfrastructure/Context/WDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WDomain.Entities;
+
+namespace WInfrastructure.Context
+{
+    /*
+    IdentityDbContext--ıdentity altyapısını kullanacagımızda bundan miras aldıgımızda ıdentity tablolarını oluşturur
+    hemde bizim tanimladigimiz tablolari olusturur.
+    */
+    public class WDbContext : DbContext
+    {
+        public WDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItems> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
