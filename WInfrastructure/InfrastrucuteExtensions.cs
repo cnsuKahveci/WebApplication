a4144dda357bd6c25f72b83714381df3e1f077aa
CKahveci <cansukahveci94@outlook.com>
2021-10-27 19:10:48 +0300

    created connection string and dbcontext
      "ConnectionStrings": {
        "PostgresConnection": "Host=localhost;Port=5432;Database=WebApplication;Username=postgres;Password=pass;Include Error Detail=true"
      },

---
+++ WInfrastructure/InfrastrucuteExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WInfrastructure.Context;
+
+namespace WInfrastructure
+{
+    public static class InfrastrucuteExtensions
+    {
+        public static IServiceCollection AddInfrastructureModule(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<WDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("PostgresConnection")));
+
+            return services;
+        }
+    }
+    
+}
